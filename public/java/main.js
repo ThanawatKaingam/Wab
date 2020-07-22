e364d2771e0efa24a0e32211a3a490df8b069a5e
ThanawatKaingam <68525371+ThanawatKaingam@users.noreply.github.com>
2020-07-23 04:39:48 +0700

    add new files
    add a bunch of new files

---
+++ public/java/main.js
@@ -0,0 +1,6 @@
+function changeColor(){
+	var container = document.getElementById("container");
+	var content = document.getElementById("content");
+	container.style.backgroundColor = "#000000";
+	content.style.color = "#FFFFFF";
+}
